54674b772b1b56dfc44c5ac534229e5f474a1342
Veninyt <1royalejohnson@gmail.com>
2025-09-01 17:18:48 -0400

    Update eclipse-client.user.js

--- eclipse-client.user.js
+++ eclipse-client.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Eclipse Client(Beta Version)
 // @namespace    http://tampermonkey.net/
-// @version      2.4
+// @version      2.5
 // @description  Have fun!
 // @author       Loqle, Venin, and others
 // @match        https://miniblox.io/
